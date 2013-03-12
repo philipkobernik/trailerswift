34b96ce2f895cbef133679ea7d3f15f0b008bf52
Philip Kobernik <ptk921@gmail.com>
2013-03-11 20:54:46 -0700

    Driving through Marana Arizona

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrailerTrail::Application.config.secret_key_base = '61d7549c53418f647fc8fca9972d3c45491c46f4296c33a643cf48918bd3454b3dd9bbdf1e24cd5b267c5829e84a676471a0a75dcc9e9e9f1f92bb10ba71cb24'
