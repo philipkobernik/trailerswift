3eda6a89d6b7a144d44a5643f150451117b99174
Philip Kobernik <ptk921@gmail.com>
2013-03-19 23:32:30 -0500

    get secrets out of the repo!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TrailerTrail::Application.config.secret_key_base = '61d7549c53418f647fc8fca9972d3c45491c46f4296c33a643cf48918bd3454b3dd9bbdf1e24cd5b267c5829e84a676471a0a75dcc9e9e9f1f92bb10ba71cb24'
+TrailerTrail::Application.config.secret_key_base = ENV['rails_secret']
