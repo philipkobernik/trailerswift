4202c3c64cb45a44470a4b39a8a716242f854c45
Philip Kobernik <ptk921@gmail.com>
2013-03-25 17:02:51 -0500

    order by located_at

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @locations = Location.all
+    @locations = Location.order('located_at ASC')
 
     @lat = @locations.last.lat
     @lng = @locations.last.lng
