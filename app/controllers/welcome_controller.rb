210df5cfbae998199b85c7a34b81a1fc01870cca
Philip Kobernik <ptk921@gmail.com>
2013-04-22 19:41:59 -0700

    remove un-used user location stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,14 +7,6 @@ class WelcomeController < ApplicationController
 
     @tour_dates = TourDate.all
 
-    @user_location = current_location = Geolocation.new(geolocation_target)
-
-    @closest_tour_date = ClosestTourDate.from current_location
-
-    center = Geocoder::Calculations.geographic_center [@user_location, @locations.last, TourDate.find(@closest_tour_date[1])]
-    user_to_center = Geocoder::Calculations.distance_between center, current_location
-    @bounding_box = Geocoder::Calculations.bounding_box(center, (user_to_center * 1.3))
-
     render 'welcome/index'
   end
 end
