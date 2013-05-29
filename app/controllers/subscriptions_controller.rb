c1d7bdae18f28be1678a50f7525f84b191350fc7
Philip Kobernik <ptk921@gmail.com>
2013-05-29 01:14:20 -0700

    come'on...

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -13,24 +13,22 @@ class SubscriptionsController < ApplicationController
     puts params
     puts request.body
 
-    if request.body.present?
-      string = URI.parse request.body
-      puts ""
-      puts " *********** STRING *********** "
-      puts ""
-      puts string
+    string = request.body.read()
+    puts ""
+    puts " *********** STRING *********** "
+    puts ""
+    puts string
 
-      puts ""
-      puts " *********** BODY *********** "
-      puts ""
-      body = JSON.parse(string)
-      puts body
+    puts ""
+    puts " *********** BODY *********** "
+    puts ""
+    body = JSON.parse(string)
+    puts body
 
-      body.each do |attrs|
-        puts "update attrs from instagram:"
-        puts attrs
-        InstagramPhoto.from_payload! attrs
-      end
+    body.each do |attrs|
+      puts "update attrs from instagram:"
+      puts attrs
+      InstagramPhoto.from_payload! attrs
     end
 
     respond_to do |format|
