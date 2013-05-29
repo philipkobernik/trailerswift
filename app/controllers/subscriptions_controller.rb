7ffa5e3d4019260ac1548ed4df202d4f06358e97
Philip Kobernik <ptk921@gmail.com>
2013-05-29 00:22:31 -0700

    MERP

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,10 +9,11 @@ class SubscriptionsController < ApplicationController
   end
 
   def callback_new_media
-    puts request.inspect
+    puts params
 
     if params[:body].present?
       body = JSON.parse(params[:body])
+
       body.each do |attrs|
         puts "update attrs from instagram:"
         puts attrs
