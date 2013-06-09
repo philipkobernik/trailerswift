1d98855387fe8ad13caae959ec07f49366460cc0
Philip Kobernik <ptk921@gmail.com>
2013-06-09 14:20:42 -0700

    enable tour_date routes to use JS geocoder

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ TrailerTrail::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  #resources :tour_dates
+  resources :tour_dates
 
   # You can have the root of your site routed with "root"
   root to: 'welcome#index'
