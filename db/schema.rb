23ea37cc2b25127c3a03b72687324b45455b33d1
Philip Kobernik <ptk921@gmail.com>
2013-07-10 12:23:25 -0700

    create july-august tour and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130609050124) do
+ActiveRecord::Schema.define(version: 20130710183906) do
 
   create_table "instagram_photos", force: true do |t|
     t.string   "tags",              array: true
