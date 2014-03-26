b21c2d749b0f6e3f67f2e872d52f36934fad8d4c
Philip Kobernik <ptk921@gmail.com>
2014-03-26 16:58:34 -0700

    remove begin of association chain for now, need to add authorization.

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,9 +1,9 @@
 class ToursController < InheritedResources::Base
   respond_to :html, :json
 
-  def begin_of_association_chain
-    current_user
-  end
+  #def begin_of_association_chain
+    #current_user
+  #end
 
   def permitted_params
     params.permit!
