9f372afdd6eb60dd83806b29830655f679d5a559
Philip Kobernik <ptk921@gmail.com>
2014-03-27 11:36:23 -0700

    tour_dates are sorted by date ASC by active record

--- app/models/tour_date.rb
+++ app/models/tour_date.rb
@@ -2,8 +2,8 @@ class TourDate < ActiveRecord::Base
   belongs_to :tour
 
   validates :date, :uniqueness => {:scope => [ :tour_id, :venue ] }
-  
-  scope :upcoming, lambda { where("date > ?", (Time.zone.now - 1.day) ) } 
+  default_scope { order('date asc') }
+  scope :upcoming, lambda { where("date > ?", (Time.zone.now - 1.day) ) }
 
   def to_coordinates
     return [self.lat, self.lng]
