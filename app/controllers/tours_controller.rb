8c3845f8b1b9f7a1cd7f6e2d430311c09639e47a
Philip Kobernik <ptk921@gmail.com>
2014-04-03 14:38:49 -0700

    tour management UI!

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,12 +1,16 @@
 class ToursController < InheritedResources::Base
   respond_to :html, :json
 
-  #def begin_of_association_chain
-    #current_user
-  #end
+  def begin_of_association_chain
+    current_user
+  end
+
+  def create
+    create! { edit_tour_url(@tour) }
+  end
 
   def permitted_params
-    params.permit!
+    params.permit(:tour => [:name])
   end
 
 end
