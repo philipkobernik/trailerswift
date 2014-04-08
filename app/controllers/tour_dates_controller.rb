a296ddc158c80aa4c2c9609c71e4434cdfc0f6cf
Philip Kobernik <ptk921@gmail.com>
2014-04-08 13:35:07 -0700

    tour dates controller inherits from resources controller
    remove CRUD views as abstract resources views will handle UI now.

--- app/controllers/tour_dates_controller.rb
+++ app/controllers/tour_dates_controller.rb
@@ -1,7 +1,15 @@
-class TourDatesController < InheritedResources::Base
+class TourDatesController < ResourcesController
   belongs_to :tour
 
   respond_to :html, :json
+
+  def attrs_for_index
+    safe_params
+  end
+
+  def attrs_for_form
+    safe_params
+  end
 
   def create
     create! do |success, failure|
@@ -11,7 +19,11 @@ class TourDatesController < InheritedResources::Base
   end
 
   def permitted_params
-    params.permit(:tour_date => [:date, :city_state, :venue, :map_query, :ticket_url, :facebook_url, :lat, :lng])
+    params.permit(:tour_date => safe_params)
   end
 
+  private
+    def safe_params
+      [:date, :city_state, :venue, :map_query, :ticket_url, :facebook_url, :lat, :lng]
+    end
 end
