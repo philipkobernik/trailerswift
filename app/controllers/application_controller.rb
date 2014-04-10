f3a63acaae6c463280d7d3147fcf813ef480bc1b
Philip Kobernik <ptk921@gmail.com>
2014-04-09 18:22:57 -0700

    error rendering for unauthorized cases, html and json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery :with => :null_session
 
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  rescue_from CanCan::AccessDenied, :with => :access_denied
 
+  def access_denied
+    respond_with do |format|
+      format.html { render file: File.join(Rails.root, 'public', '401.html'), status: 401 }
+      format.json { render json: { message: "access denied" }, status: 401 }
+    end
+  end
   def not_found
-    render file: File.join(Rails.root, 'public', '404.html'), status: 404, layout: false
+    render file: File.join(Rails.root, 'public', '404.html'), status: 404
   end
 
   def session_logged_in?
