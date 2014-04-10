30048d847644c8435f789e05f1e49a0ee0d7885c
Philip Kobernik <ptk921@gmail.com>
2014-04-09 21:59:49 -0700

    tours#index uses accessible_by || redirects to root

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,11 +1,16 @@
 class ToursController < InheritedResources::Base
   respond_to :html, :json
 
-  load_and_authorize_resource :tour, except: :show
+  load_and_authorize_resource :tour, except: [:show, :index]
 
   #def begin_of_association_chain
     #current_user
   #end
+
+  def index
+    redirect_to root_path unless session_logged_in?
+    @tours = Tour.accessible_by(current_ability, :update)
+  end
 
   def create
     create! { edit_tour_url(@tour) }
--- spec/controllers/tours_controller_spec.rb
+++ spec/controllers/tours_controller_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
 describe ToursController do
-
-
   before do
     @user = create :user
     controller.stubs(:current_user).returns @user
 
-    #@ability = Object.new.extend(CanCan::Ability)
-    #controller.stubs current_ability: @ability
+    @ability = Object.new.extend(CanCan::Ability)
+    controller.stubs current_ability: @ability
   end
 
   let(:tour) { create :tour, user: @user }
@@ -40,6 +38,29 @@ describe ToursController do
       it 'authorizes resource' do
         controller.expects(:authorize!).with(:destroy, tour).returns true
         delete :destroy, { id: tour.id }
+      end
+    end
+  end
+
+  context 'GET index' do
+    context 'when session is logged in' do
+      before do
+        controller.stubs(:session_logged_in?).returns true
+      end
+
+      it 'loads tours via :accessible_by' do
+        Tour.expects(:accessible_by).with(@ability, :update).returns [create(:tour)]
+        get :index
+      end
+    end
+
+    context 'when session is NOT logged in' do
+      before do
+        controller.stubs(:session_logged_in?).returns false
+      end
+
+      it 'redirects to root_path' do
+        expect(get(:index)).to redirect_to root_path
       end
     end
   end
